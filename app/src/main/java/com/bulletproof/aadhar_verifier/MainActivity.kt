d59353569cc97b39197e4b9b147f4bfeaae34f5f
Rajin Gangadharan <rajin.gangadharan@gmail.com>
2021-11-19 22:33:51 +0530

    Prepared for finals

--- app/src/main/java/com/bulletproof/aadhar_verifier/MainActivity.kt
+++ app/src/main/java/com/bulletproof/aadhar_verifier/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.bulletproof.aadhar_verifier
 
 import android.content.Intent
+import android.nfc.NdefMessage
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,38 @@ class MainActivity : AppCompatActivity() {
         btnSave.setOnClickListener {
             val intent = Intent(this, ScanActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+            processIntent(intent)
+        }
+    }
+
+    private fun processIntent(intent: Intent) {
+        intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)?.also { rawMsgs ->
+            (rawMsgs[0] as NdefMessage).apply {
+                try {
+                    val obj = JSONObject(String(records[0].payload))
+                    if(obj.getString("name") == "") {
+                        val i = Intent(this@MainActivity, UnverifiedActivity::class.java)
+                        startActivity(i)
+                    } else {
+                        val i = Intent(this@MainActivity, VerifiedActivity::class.java)
+                        startActivity(i)
+                    }
+                } catch(e: Exception) {
+                    val i = Intent(this@MainActivity, UnverifiedActivity::class.java)
+                    startActivity(i)
+                }
+            }
         }
     }
 }
